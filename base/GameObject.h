ad63289f4a6e713bb0ec1304a5082f508d931361
GelerCAT <135972531+Gelercatty@users.noreply.github.com>
2025-08-20 17:49:45 +0800

    重构2.0
    引入imgui glfw 重构引擎

--- base/GameObject.h
+++ base/GameObject.h
@@ -3,11 +3,9 @@
 #include "../utils/geometry.h"
 #include "../base/Mesh.h"
 
-
 class GameObject
 {
 public:
 	Transform transform; // transform in world space
-	Mesh mesh; // mesh of the object
-	
+	Mesh mesh;			 // mesh of the object
 };
